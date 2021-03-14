07da3e25bb2248aff82ab7f33251844a7cec5464
unknown <rohitgalande45@gmail.com>
2021-03-14 11:38:56 -0700

    File changes

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const url = process.env.MONGO_URI || 'mongodb://localhost:menu';
 const connection = mongoose.connection
 
 app.use(express.static("public"))
+app.use('/css',express.static(__dirname + "public/css"))
+app.use('/js',express.static(__dirname + "public/js"))
+app.use('/img',express.static(__dirname + "public/img"))
+
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 .then(()=>{
